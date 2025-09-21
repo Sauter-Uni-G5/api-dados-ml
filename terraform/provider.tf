ca053ca1034e90574d079bc037d7a91b10f55dc7
Luzi.Alcielma <62629383+Alcielma@users.noreply.github.com>
2025-09-21 16:43:34 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,18 @@
-provider "google" {
-  credentials = file("terraform-key.json")
-  project     = var.project
-  region      = var.region
-}
-
 terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"
+    }
+  }
+
   backend "gcs" {
-    credentials = "terraform-key.json"
     bucket      = "terraform-sauter-university"
     prefix      = "terraform/state"
   }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
 }
